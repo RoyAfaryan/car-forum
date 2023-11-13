169851f17d66e72f35dea7a3f4b0c8447d263332
RoyAfaryan <royafaryan@gmail.com>
2023-11-12 21:30:05 -0800

    created login authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  Button,
+  Flex,
+  Heading,
+  Text,
+  TextField,
+  View,
+  withAuthenticator,
+  Image,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({signOut}) {
   return (
     <div>
       <h1>Hello World!</h1>
-     
+      <Button onClick={signOut}>Sign Out</Button>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
